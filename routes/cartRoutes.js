c0dd27e2b75fb14f2c475802db295c038a6c4670
Ishaan Vashishth <ishaan.vashishth@csdevents.com>
2025-12-17 17:04:27 +0530

    updated cart

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const { isLoggedIn } = require("../middleware/tokenVerification");
+const cartIdentifier = require("../middleware/cartIdentifier");
 const {
     getCart,
     addToCart,
     removeFromCart,
-    updateCartItemQuantity,
+    updateQuantity,
 } = require("../controllers/cartController");
 
+router.use(cartIdentifier);
 router.get("/", getCart);
 router.post("/add", addToCart);
 router.delete("/remove/:productId", removeFromCart);
-router.put("/update", updateCartItemQuantity);
+router.put("/update", updateQuantity);
 
 module.exports = router;
